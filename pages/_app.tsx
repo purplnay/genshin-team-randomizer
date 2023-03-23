288a9a4697409c90aeae9a60d70b692930b7fa52
Nay <emilydevoos@gmail.com>
2023-03-23 10:24:18 +0100

    Add global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '~/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "~/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Poppins } from "next/font/google";
+import clsx from "clsx";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500"],
+  variable: "--font-poppins",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={clsx(poppins.variable, "font-sans")}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
